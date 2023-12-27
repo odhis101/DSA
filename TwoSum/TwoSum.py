a0af78fdf765539925e085efa545cc47791325f1
odhis101 <joshodhiambo5@gmail.com>
2023-12-27 11:03:34 +0300

    two sum code

--- TwoSum/TwoSum.py
+++ TwoSum/TwoSum.py
@@ -8,7 +8,14 @@ def two_sum(nums, target):
                 return [i, j]
     return []  # No solution found
 
-
+def two_sum_optimal(nums, target):
+    num_indices = {}  # Dictionary to store the indices of numbers
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_indices:
+            return [num_indices[complement], i]
+        num_indices[num] = i
+    return []  # No solution found
 # Test the function
 nums = [2, 7, 11, 15]
 target = 9
