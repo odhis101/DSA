d6772c3d12b6f668522ec0f893f32989e0c91b2a
odhis101 <joshodhiambo5@gmail.com>
2023-12-27 11:10:46 +0300

    fixed small issues

--- ReverseString/ReverseString.py
+++ ReverseString/ReverseString.py
@@ -2,24 +2,16 @@ def reverse_string(s):
     return s[::-1]
 
 def reverse_string_complex(s):
-    # Initialize an empty string to store the reversed result
     reversed_string = ""
 
-    # Iterate over each character in the original string in reverse order
     for i in range(len(s) - 1, -1, -1):
-        # Append the current character to the reversed string
         reversed_string += s[i]
 
-    # Return the reversed string
     return reversed_string
 
 # Test the function
 input_str = "hello"
 result = reverse_string(input_str)
 print(result)
-
-
-# Test the function
-input_str = "hello"
-result = reverse_string(input_str)
-print(result)
+result1 = reverse_string_complex(result)
+print(result1)
