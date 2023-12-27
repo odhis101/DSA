c6ff5f5bf005e3b44dca813a15bb0eab1f94d613
odhis101 <joshodhiambo5@gmail.com>
2023-12-27 18:44:08 +0300

    best solution

--- Rotate_Array/rotateArray.py
+++ Rotate_Array/rotateArray.py
@@ -2,9 +2,18 @@ def rotateArray(nums, k):
     n = len(nums)
     k = k % n
 
-    for i in range(k):
-        last_element = nums.pop()
-        nums.insert(0, last_element)
+    # Helper function to reverse a portion of the array
+    def reverse(arr, start, end):
+        while start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+            start += 1
+            end -= 1
+
+    reverse(nums, 0, n - 1)
+    reverse(nums, 0, k - 1)
+
+    # Reverse the remaining elements
+    reverse(nums, k, n - 1)
 
 # Test the function
 nums = [1, 2, 3, 4, 5, 6, 7]
