a7e14a4d93c4142b398a37ae2b89df67574531af
odhis101 <joshodhiambo5@gmail.com>
2023-12-27 15:50:19 +0300

    validParenthesis with a working solution

--- ValidParenthesis/validParenthesis.py
+++ ValidParenthesis/validParenthesis.py
@@ -1,38 +1,18 @@
 def isValidParentheses(s):
-    def isMatching(left, right):
-        return (left == '(' and right == ')') or \
-               (left == '{' and right == '}') or \
-               (left == '[' and right == ']')
+    stack = []
+    bracket_mapping = {')': '(', '}': '{', ']': '['}
 
-    left = 0
-    right = len(s) - 1
+    for char in s:
+        if char in bracket_mapping.values():  # Opening bracket
+            stack.append(char)
+        elif char in bracket_mapping.keys():  # Closing bracket
+            if not stack or stack.pop() != bracket_mapping[char]:
+                return False  # Mismatched brackets
 
-    while left < right:
-        # Skip non-bracket characters from the left
-        while left < right and s[left] not in '(){}[]':
-            left += 1
-        
-        # Skip non-bracket characters from the right
-        while left < right and s[right] not in '(){}[]':
-            right -= 1
-
-        # If both pointers are not on brackets, continue
-        if s[left] not in '(){}[]' or s[right] not in '(){}[]':
-            left += 1
-            right -= 1
-            continue
-        
-        # If the brackets don't match, return False
-        if not isMatching(s[left], s[right]):
-            return False
-        
-        left += 1
-        right -= 1
-
-    return True
+    return not stack  
 
 # Test the function
-print(isValidParentheses("()"))      # True
-print(isValidParentheses("()[]{}"))  # True
-print(isValidParentheses("(]"))      # False
-print(isValidParentheses("([)]"))    # False
+print(isValidParentheses("()"))    
+print(isValidParentheses("()[]{}"))
+print(isValidParentheses("(]"))      
+print(isValidParentheses("([)]"))    
