4bb810c7ebf2327aa6e81ba0c72a0098f29c86af
odhis101 <joshodhiambo5@gmail.com>
2023-12-28 17:33:17 +0300

    code for 3 sum most optimal solution

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -30,6 +30,47 @@ def threeSum(nums):
 
     return result
 
+
+def threeSumOptimal(nums):
+    if len(nums) < 3:
+        return []
+
+    # Sort the array
+    nums.sort()
+
+    result = []
+    for i in range(len(nums) - 2):
+        # Skip duplicates to avoid duplicate triplets
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        left, right = i + 1, len(nums) - 1
+
+        while left < right:
+            # Calculate the current sum
+            current_sum = nums[i] + nums[left] + nums[right]
+
+            if current_sum == 0:
+                result.append([nums[i], nums[left], nums[right]])
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+
+                # Move the pointers
+                left += 1
+                right -= 1
+
+            elif current_sum < 0:
+                # Increase the sum by moving the left pointer to the right
+                left += 1
+            else:
+                # Decrease the sum by moving the right pointer to the left
+                right -= 1
+
+    return result
+
+
 # Test the function
 nums = [-1, 0, 1, 2, -1, -4]
 print(threeSum(nums))  # Output: [[-1, -1, 2], [-1, 0, 1]]
+print(threeSumOptimal(nums))  # Output: [[-1, -1, 2], [-1, 0, 1]]
