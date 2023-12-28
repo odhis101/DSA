4ba83b29855a4485e3c1f0fed4059bea15bd4154
odhis101 <joshodhiambo5@gmail.com>
2023-12-28 10:30:01 +0300

    majority element boyer algo

--- majorityElement:/majorityElement.py
+++ majorityElement:/majorityElement.py
@@ -16,10 +16,30 @@ def majorityElement(nums):
     majority_element = max(count_dict, key=count_dict.get)
 
     return majority_element
+def majorityElement_effecient(nums):
+    candidate = None
+    count = 0
 
+    # First pass: Find a potential majority element
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += 1 if num == candidate else -1
+
+    # Second pass: Verify if the candidate is the majority element
+    count = 0
+    for num in nums:
+        if num == candidate:
+            count += 1
+
+    # Check if the candidate is the majority element
+    return candidate if count > len(nums) // 2 else None
 # Test the function
 nums1 = [3, 3, 4, 2, 4, 4, 2, 4, 4]
 nums2 = [2, 2, 1, 1, 1, 2, 2]
 
 print(majorityElement(nums1))  # Output: 4
 print(majorityElement(nums2))  # Output: 2
+
+print(majorityElement_effecient(nums1))  # Output: 4   
+print(majorityElement_effecient(nums2))  # Output: 2
