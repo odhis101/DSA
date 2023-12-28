80163c9c6ec36acfa3d2d7e5a12f2bb4bc1171ef
odhis101 <joshodhiambo5@gmail.com>
2023-12-28 16:55:47 +0300

    complexity analysis

---
+++ LongestCommonPrefix/longestCommonPrefix.py
@@ -0,0 +1,29 @@
+def longestCommonPrefix(strs):
+    # Edge case: If the array is empty, return an empty string
+    if not strs:
+        return ""
+
+    # Split the first word into characters
+    first_word = list(strs[0])
+
+    # Initialize the result string
+    result = ""
+
+    # Iterate over characters in the first word
+    for i in range(len(first_word)):
+        # Check if the current character is common to all words
+        if all(word[i] == first_word[i] for word in strs):
+            # Append the character to the result string
+            result += first_word[i]
+        else:
+            # If characters differ, break the loop
+            break
+
+    return result
+
+# Test the function
+strs1 = ["flower", "flow", "flight"]
+strs2 = ["dog", "racecar", "car"]
+
+print(longestCommonPrefix(strs1))  # Output: "fl"
+print(longestCommonPrefix(strs2))  # Output: ""
