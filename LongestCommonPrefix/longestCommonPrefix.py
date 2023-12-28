5a0a0d2356196aac5899679a5c8cc928625c7864
odhis101 <joshodhiambo5@gmail.com>
2023-12-28 17:09:00 +0300

    longest common prefix optimized

--- LongestCommonPrefix/longestCommonPrefix.py
+++ LongestCommonPrefix/longestCommonPrefix.py
@@ -18,6 +18,26 @@ def longestCommonPrefix(strs):
             break
 
     return result
+def longestCommonPrefixOptimized(strs):
+    # Edge case: If the array is empty, return an empty string
+    if not strs:
+        return ""
+
+    # Use zip to group characters from the same position together
+    for i, group in enumerate(zip(*strs)):
+        # Check if all characters in the group are the same
+        if len(set(group)) == 1:
+            continue
+        else:
+            # If a mismatch is found, return the common prefix up to this point
+            return strs[0][:i]
+
+    # If no mismatch is found, return the entire first word as the common prefix
+    return strs[0]
+
+# Test the function
+
+
 
 # Test the function
 strs1 = ["flower", "flow", "flight"]
@@ -25,3 +45,6 @@ strs2 = ["dog", "racecar", "car"]
 
 print(longestCommonPrefix(strs1))  # Output: "fl"
 print(longestCommonPrefix(strs2))  # Output: ""
+
+print(longestCommonPrefixOptimized(strs1))  # Output: "fl"
+print(longestCommonPrefixOptimized(strs2))  # Output: ""
